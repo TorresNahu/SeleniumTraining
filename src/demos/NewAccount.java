aeda88736001fab5b3b3c5b68cf7dc1bdbd9b4ca
ntorres <torres.nahu@gmail.com>
2019-01-25 17:46:44 -0300

    Se termino de agregar los elementos que faltaban para completar el test de la clase. Se agregaron los checkbox y la confirmacion de mensaje.

--- src/demos/NewAccount.java
+++ src/demos/NewAccount.java
@@ -19,44 +19,99 @@ public class NewAccount {
         String name = "Mari Sue";
         String email = "ms@testemail.com";
         String phoneNumber = "123987123";
-        String gender;
+        String gender = "Female";
         String password = "mspass";
         String country = "Brazil";
-        String weeklyEmail;
-        String monthlyEmail;
-        String occasional;
+        boolean weeklyEmail = true;
+        boolean monthlyEmail = false;
+        boolean occasional = false;
+        String successfulMessage = "Customer information added successfully";
 
         //WebElements definitions
         WebElement createAccountButtonElement = driver.findElement(By.xpath("//*[@id=\"ctl01\"]/div[3]/div[2]/div/div[2]/a"));
         createAccountButtonElement.click();
 
-        WebElement nameElement = driver.findElement(By.id("MainContent_txtFirstName"));
-        WebElement emailElement = driver.findElement(By.id("MainContent_txtEmail"));
-        WebElement phoneElement = driver.findElement(By.id("MainContent_txtHomePhone"));
-        WebElement passwordElement = driver.findElement(By.id("MainContent_txtPassword"));
-        WebElement verifyPasswordElement = driver.findElement(By.id("MainContent_txtVerifyPassword"));
-        WebElement countryElement = driver.findElement(By.id("MainContent_menuCountry"));
-        WebElement submitButtonElement = driver.findElement(By.id("MainContent_btnSubmit"));
+        WebElement txtName = driver.findElement(By.id("MainContent_txtFirstName"));
+        WebElement txtEmail = driver.findElement(By.id("MainContent_txtEmail"));
+        WebElement txtPhone = driver.findElement(By.id("MainContent_txtHomePhone"));
+        WebElement txtPassword = driver.findElement(By.id("MainContent_txtPassword"));
+        WebElement txtVerifyPassword = driver.findElement(By.id("MainContent_txtVerifyPassword"));
+        WebElement txtCountry = driver.findElement(By.id("MainContent_menuCountry"));
+        WebElement btnSubmit = driver.findElement(By.id("MainContent_btnSubmit"));
+        WebElement maleRadioButton = driver.findElement(By.id("MainContent_Male"));
+
+        WebElement femaleRadioButton = driver.findElement(By.id("MainContent_Female"));
+        WebElement weeklyCheckbox = driver.findElement(By.id("MainContent_checkWeeklyEmail"));
+        WebElement monthlyCheckbox = driver.findElement(By.id("MainContent_checkMonthlyEmail"));
+        WebElement occasionalCheckbox = driver.findElement(By.id("MainContent_checkUpdates"));
+
+        WebElement successfulMessageElement;
 
         //Fill out the form
-        nameElement.sendKeys(name);
-        emailElement.sendKeys(email);
-        phoneElement.sendKeys(phoneNumber);
-        passwordElement.sendKeys(password);
-        verifyPasswordElement.sendKeys(password);
+        txtName.sendKeys(name);
+        txtEmail.sendKeys(email);
+        txtPhone.sendKeys(phoneNumber);
+        txtPassword.sendKeys(password);
+        txtVerifyPassword.sendKeys(password);
 
         //How to manage the dropdown.
-        new Select(countryElement).selectByVisibleText(country);
+        new Select(txtCountry).selectByVisibleText(country);
 
-        driver.findElement(By.id("MainContent_Female")).click();
-        driver.findElement(By.id("MainContent_checkWeeklyEmail")).click();
+        //Gender radio button
+        if (gender.equalsIgnoreCase("Male")) {
+            maleRadioButton.click();
+        } else if (gender.equalsIgnoreCase("Female")) {
+            femaleRadioButton.click();
+        }
+
+        //Check box management
+        //1. Weekly checkbox
+        if (weeklyEmail) {
+            if (!weeklyCheckbox.isSelected()) {
+                weeklyCheckbox.click();
+            }
+        } else {
+            if (weeklyCheckbox.isSelected()) {
+                weeklyCheckbox.click();
+            }
+        }
+
+        //2.Monthly checkbox
+        if (monthlyEmail) {
+            if (!monthlyCheckbox.isSelected()) {
+                monthlyCheckbox.click();
+            }
+        } else {
+            if (monthlyCheckbox.isSelected()) {
+                monthlyCheckbox.click();
+            }
+        }
+
+        //3.Occasional checkbox
+        if (occasional) {
+            if (!occasionalCheckbox.isSelected()) {
+                occasionalCheckbox.click();
+            }
+        } else {
+            if (occasionalCheckbox.isSelected()) {
+                occasionalCheckbox.click();
+            }
+        }
 
         //Submit the form
-        submitButtonElement.click();
+        btnSubmit.click();
 
         //Get confirmation & close browser
-        String conf = driver.findElement(By.id("MainContent_lblTransactionResult")).getText();
-        System.out.println("Confirmation: " + conf);
-//        driver.close();
+        successfulMessageElement = driver.findElement(By.id("MainContent_lblTransactionResult"));
+        String conf = successfulMessageElement.getText();
+
+        //Validate result of the test
+        if (conf.contains(successfulMessage)) {
+            System.out.println("Message: " + conf);
+        } else {
+            System.out.printf("Test Fail");
+        }
+
+        driver.close();
     }
 }
